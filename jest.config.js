3c4e1055857024e4828a77ec3986464bc5e6e73c
Dan Hunt <dkhunt27@gmail.com>
2021-10-01 14:49:53 -0400

    more cicd work

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest'
   },
-  moduleFileExtensions: [
-    "ts",
-    "js",
-    "json"
-  ],
-  testMatch: [
-    '**/tests/**/*.(ts|tsx)',
-    '**/?(*.)+(spec|test).(ts|tsx)',
-  ],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/tests/**/*.(ts|tsx)', '**/?(*.)+(spec|test).(ts|tsx)'],
   coverageDirectory: '.coverage',
-  collectCoverageFrom: [
-    'lib/**/*.{ts,tsx,js,jsx}',
-    '!lib/**/*.d.ts',
-    '!lib/**/index.ts'
-  ],
+  collectCoverageFrom: ['lib/**/*.{ts,tsx,js,jsx}', '!lib/**/*.d.ts', '!lib/**/index.ts'],
   coverageThreshold: {
     global: {
       branches: 100,
@@ -26,5 +15,5 @@ module.exports = {
       statements: 100
     }
   },
-  collectCoverage: true
+  collectCoverage: false
 };
